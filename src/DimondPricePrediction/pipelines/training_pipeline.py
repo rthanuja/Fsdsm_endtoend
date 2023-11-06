954c78dbac53d25dc6a1033d0d8116ccdcbe2edb
Thanuja Ramisetty <rthanuja.ramisetty@gmail.com>
2023-11-06 13:46:15 +0530

    updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,4 +1,6 @@
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
+from src.DimondPricePrediction.components.data_transformation import DataTransformation
+from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 
 import os
 import sys
@@ -8,4 +10,12 @@ import pandas as pd
 
 obj=DataIngestion()
 
-obj.initiate_data_ingestion()
+train_data_path,test_data_path = obj.initiate_data_ingestion()
+
+DataTransformation_obj=DataTransformation()
+
+train_arr,test_arr = DataTransformation_obj.initialize_data_transformation(train_data_path,test_data_path)
+
+ModelTrainer_obj =ModelTrainer()
+
+ModelTrainer_obj.initiate_model_training(train_arr,test_arr)
